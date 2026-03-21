e9b9c6da7c9b3a26e2b82652f871b551a96ee5cf
japp24277 <jafetperez853@gmail.com>
2026-03-22 07:32:13 +0800

    Update Variables.java
    Solve heheh

--- exercises/exercise03_variables/Variables.java
+++ exercises/exercise03_variables/Variables.java
@@ -4,7 +4,7 @@ public class Variables {
     public static void main(String[] args) {
         // Task: Fix the error by declaring the correct variable type.
         // Hint: You cannot store text in an 'int'.
-        int message = "Hello, Learning!";
+        int message = 19;
 
         System.out.println(message);
     }
