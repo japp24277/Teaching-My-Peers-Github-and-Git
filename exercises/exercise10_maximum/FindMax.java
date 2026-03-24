5956901ad6b5a37995e42229ca0b71a0b0fe9bfe
japp24277 <jafetperez853@gmail.com>
2026-03-24 18:38:15 +0800

    Update FindMax.java
    Solve

--- exercises/exercise10_maximum/FindMax.java
+++ exercises/exercise10_maximum/FindMax.java
@@ -3,12 +3,14 @@ package exercises.exercise10_maximum;
 public class FindMax {
     public static void main(String[] args) {
         int x = 18;
+        int y =19;
 
         // Task: Write an if-else statement that finds and prints 
         // the greater value between x and y
         if (x > y) {
-            System.out.println(x)
+            System.out.println(x);
         } else {
-            System.out.println(y)
+            System.out.println(y);
     }
+}
 }
