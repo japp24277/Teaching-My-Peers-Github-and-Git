a5ae7f64ae09e4c1839e7a376c8c3ccc2ce40ee5
japp24277 <jafetperez853@gmail.com>
2026-03-24 18:34:55 +0800

    Update SignCheck.java
    Solve

--- exercises/exercise09_positive_negative/SignCheck.java
+++ exercises/exercise09_positive_negative/SignCheck.java
@@ -2,16 +2,17 @@ package exercises.exercise09_positive_negative;
 
 public class SignCheck {
     public static void main(String[] args) {
-        int num = -7;
+        int num = 7;
 
         // Task: Write an if-else statement that prints "Positive" 
         // if num is greater than 0, "Negative" if less than 0, 
         // otherwise print "Zero"
         if (num !> 0) {
-            System.out.println("Positive")
-
-            System.out.println("Negative")
-        } else
-            System.out.println("Zero")
+         System.out.println("Positive");
+      } else if {
+         System.out.println("Negative");
+      } else {
+         System.out.println("Zero");
     }
+}
 }
