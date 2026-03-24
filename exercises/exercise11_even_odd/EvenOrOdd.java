c76ec5dd6d852d45b99d76291723e8652962b10f
japp24277 <jafetperez853@gmail.com>
2026-03-24 18:52:21 +0800

    Update EvenOrOdd.java
    Solve

--- exercises/exercise11_even_odd/EvenOrOdd.java
+++ exercises/exercise11_even_odd/EvenOrOdd.java
@@ -3,11 +3,17 @@ package exercises.exercise11_even_odd;
 public class EvenOrOdd {
     public static void main(String[] args) {
         int number = 23;
-
+        /* I never try modulo on program
+          but I will try my best
+            */
+    
         // Task: Write an if-else statement that prints "Even" 
         // if number is even, otherwise print "Odd"
         // Hint: Use modulo operator %
-            else
+        if(number % 1==1) {
+           System.out.prinln("Even");
+    } else{
             System.out.println("Odd");
     }
+}
 }
